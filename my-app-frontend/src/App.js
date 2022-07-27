3216cfca7a810800a3c29ff48c0674aa2382d371
Kale Leach <kleach1212@gmail.com>
2022-07-26 19:12:11 -0700

    Styling Head

---
+++ my-app-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Header/Header';
+import 
+
+function App() {
+  return (
+    <div id = "App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app-frontend/src/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header(){
+
+  return(
+    <div className= "header">
+      <h1> JobHunt <FontAwesomeIcon icon="fa-solid fa-address-card" /> </h1>
+    </div>
+  )
+}
+
+export default Header;
