6818d7d087e210b24417e8596e3ee915e33d85f5
Kale Leach <kleach1212@gmail.com>
2022-08-01 08:03:00 -0700

    Commit 13: Handle Job Form

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css'
 import Header from './Header/Header';
 import MainComponent from './MainComponent/MainComponent';
 
+
+
+
 function App() {
   return (
     <div id = "App">
       <Header/>
-      <MainComponent/>
+      <MainComponent />
     </div>
   );
 }
--- my-app-frontend/src/MainComponent/JobForm.js
+++ my-app-frontend/src/MainComponent/JobForm.js
@@ -3,43 +3,50 @@ import './JobForm.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 
-function JobForm(){
+function JobForm({posName}){
   const [formOpen, setFormOpen] = useState(false)
+  const [isPosition, setPosition ] = useState("")
+  const [isCompany, setCompany] = useState("")
+
+  function handlePostion(e){
+    setPosition(e.target.value)
+    console.log(isPosition)
+  }
+
+  function handleCompany(e){
+    setCompany(e.target.value)
+    console.log(isCompany)
+  }
 
   let handleOpenForm = () => {
     setFormOpen(!formOpen)
     console.log(formOpen)
   }
 
-  const Form = () => (
-    <div  className="active_form">
-      <label>Positon</label>
-      <input type='text' id = 'position'/>
-      <label>Company </label>
-      <input type='text' id = 'position'/>
-      <label>Status </label>
-      <select id = 'status'>
-        <option value ='Apply'> Apply</option>
-        <option value ='Applied'> Applied</option>
-        <option value ='Contacted'> Contacted</option>
-        <option value ='Interview'> Interview</option>
-        <option value ='Accepted'> Accepted</option>
-        <option value ='Rejected'> Rejected</option>
-      </select>
-      <label> Date </label>
-      <input type='date' id = 'position'/>
-      <input id = 'submit' type ='submit' />
-      
-    </div>
-  )
 
   return(
     <div className='form'>
       <FontAwesomeIcon icon={faSquarePlus}  className = 'add' onClick={handleOpenForm}/>  <span> Add a Job</span>
-      <div >
-      {formOpen ? <Form/> : null}
-      </div>
-
+      {formOpen ?  <div  className="active_form">
+        <label>Positon</label>
+        <input type='text' id = 'position' onChange = {handlePostion} value = {isPosition}/>
+        <label>Company </label>
+        <input type='text' id = 'position' onChange={handleCompany} value = {isCompany}/>
+        <label>Status </label>
+        <select id = 'status'>
+        <option value =''> Choose Status</option>
+          <option value ='Apply'> Apply</option>
+          <option value ='Applied'> Applied</option>
+          <option value ='Contacted'> Contacted</option>
+          <option value ='Interview'> Interview</option>
+          <option value ='Accepted'> Accepted</option>
+          <option value ='Rejected'> Rejected</option>
+        </select>
+        <label> Date </label>
+        <input type='date' id = 'position'/>
+        <input id = 'submit' type ='submit' />
+        </div> : null
+      }
     </div>
   )
 }
