ab200fd171d7764cd934c7d0cb6fd3c8cdd894ff
Kale Leach <kleach1212@gmail.com>
2022-08-01 17:45:11 -0700

    Commit 16: Added Post to Reg Users

--- my-app-frontend/src/Header/Register.js
+++ my-app-frontend/src/Header/Register.js
@@ -1,12 +1,17 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import './Register.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Register(){
   const [isUsername, setUsername] = useState("")
-  const [isPassword, setPassword] = useState("")
-  const [isPasswordCorr, setPasswordCorr] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordCorr, setPasswordCorr] = useState("")
+  // const [showErrorMessage, setShowErrorMessage] = useState(false);
+  // const [cPasswordClass, setCPasswordClass] = useState('form-control');
+  const [isCPasswordDirty, setIsCPasswordDirty] = useState(false);
   
+
 
   function handleUsername(e){
     setUsername(e.target.value)
@@ -15,42 +20,56 @@ function Register(){
 
   function handlePassword(e){
     setPassword(e.target.value)
-    console.log(isPassword)
+    console.log(password)
   }
 
   function handlePasswordCorrect(e){
     setPasswordCorr(e.target.value)
-    console.log(isPasswordCorr)
+    setIsCPasswordDirty(true)
+    console.log(passwordCorr)
   }
+
+  
 
   function handleSubmit(e){
     e.preventDefault()
     const formData = {
       username: isUsername,
-      password: isPassword
+      password: password
     }
 
-    fetch("http://localhost:9292/users/signup",{
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify(formData)
-    })
-
-    
+    if(password === passwordCorr){
+      fetch("http://localhost:9292/users/signup",{
+        method:"POST",
+        headers:{
+          "Content-Type": "application/json",
+        },
+        body:JSON.stringify(formData)
+      })
+    } else{
+      alert("Passwords do not match")
+    }
+    setUsername("")
+    setPassword("")
+    setPasswordCorr("")
   }
   
   return(
-    <div className='reg-cont'>
-      <label>Create Username</label>
-      <input type='text' onChange = {handleUsername} value = {isUsername}/>
-      <label>Password</label>
-      <input type='password' onChange = {handlePassword} value = {isPassword}/>
-      <label>Confirm Password</label>
-      <input type='password' onChange = {handlePasswordCorrect} value = {isPasswordCorr}/>
-      <button className = 'sign-up'>Sign Up </button>
-  </div>
+    <form className='reg-cont' onSubmit={handleSubmit}>
+      <div >
+        <label> Create Username </label>
+        <input type='text' onChange = {handleUsername} value = {isUsername}/>
+      </div>
+      <div className='form--group'> 
+        <label> Password </label>
+        <input className = "form-control" type='password' onChange = {handlePassword} value = {password}/>
+      </div>
+      <div className='form--group'>
+        <label>Confirm Password</label>
+        <input className = "form-control" type='password' onChange = {handlePasswordCorrect} value = {passwordCorr}/>
+      </div>
+      <button className = 'sign-up'> Sign Up </button>
+  </form>
   )
 }
 
