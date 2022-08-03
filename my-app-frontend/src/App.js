23c75640bbe3a7f195fabfe09d3b771dae619a7a
Kale Leach <kleach1212@gmail.com>
2022-08-03 12:46:48 -0700

    Commit 21: Added mapping to grid

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Header from './Header/Header';
 import MainComponent from './MainComponent/MainComponent';
 
 
 function App() {
+  const [jobs , setJobs] = useState([])
+
+  useEffect(() =>{
+    fetch('http://localhost:9292//all')
+    .then(res => res.json())
+    .then(data => {
+      setJobs(data)
+      console.log(data)
+    })
+  },[])
+
+
   return (
     <div id = "App">
       <Header/>
-      <MainComponent />
+      <MainComponent  jobsList = {jobs}/>
     </div>
   );
 }
--- my-app-frontend/src/MainComponent/MainComponent.js
+++ my-app-frontend/src/MainComponent/MainComponent.js
@@ -2,25 +2,24 @@ import React, {useState} from 'react';
 import './MainComponent.css'
 import JobForm from './JobForm';
 import Button from '@mui/material/Button';
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
-function MainComponent(){
+function MainComponent({jobsList}){
   const [checkboxSelection, setCheckboxSelection] = useState(true);
 
-  const rows: GridRowsProp[] = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-  ];
+  // const rows: GridRowsProp[] = [
+  //   { id: 1, col1: 'Hello', col2: 'World' },
+  //   { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  //   { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  // ];
   
-  const columns: GridColDef = [
-    { field: 'col1', headerName: 'Position', width: 200 , editable: true },
-    { field: 'col2', headerName: 'Company Name', width: 200, editable: true  },
-    { field: 'col3', headerName: 'Status', width: 150, editable: true, type: 'singleSelect' , valueOptions: ['Apply','Applied','Contacted', 'Interview', 'Rejected', 'Accepted']},
-    { field: 'col4', headerName: 'Applied Date', width: 150, editable: true, type: 'dateTime' },
-    { field: 'col5', headerName: 'Response Date', width: 150, editable: true, type: 'dateTime' },
-    { field: 'col6', headerName: 'Interview Date', width: 150, editable: true, type: 'dateTime' },
-    { field: 'col7', headerName: 'Notes', width: 500, editable: true, type: 'string' }
+  const columns: GridColDef[] = [
+    { field: 'position', headerName: 'Position', width: 200 , editable: true },
+    { field: 'company', headerName: 'Company Name', width: 200, editable: true  },
+    { field: 'status', headerName: 'Status', width: 150, editable: true, type: 'singleSelect' , valueOptions: ['Apply','Applied','Contacted', 'Interview', 'Rejected', 'Accepted']},
+    { field: 'applieddate', headerName: 'Applied Date', width: 150, editable: true, type: 'dateTime' },
+    { field: 'responsedate', headerName: 'Response Date', width: 150, editable: true, type: 'dateTime' },
+    { field: 'notes', headerName: 'Notes', width: 500, editable: true, type: 'string' }
     // { field: 'col7', headerName: 'Actions', width: 100, editable: true, type: 'actions'}
     
   ];
@@ -41,7 +40,7 @@ function MainComponent(){
           fontFamily:'Arvo',
           fontColor:'#000000'
         }} 
-          class = 'grid' rows={rows} columns={columns} checkboxSelection={checkboxSelection} experimentalFeatures={{ newEditingApi: true }
+          class = 'grid' rows={jobsList} columns={columns} checkboxSelection={checkboxSelection} experimentalFeatures={{ newEditingApi: true }
           }/>
       </div>
     </div>
